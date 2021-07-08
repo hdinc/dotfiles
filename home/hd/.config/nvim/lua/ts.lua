42c7d1a84932ed5ca3c9c8eaf62211529cd94900
hdinc <macellan091@gmail.com>
2021-07-08 03:42:08 +0300

    nvim disabled treesitter indent

--- home/hd/.config/nvim/lua/ts.lua
+++ home/hd/.config/nvim/lua/ts.lua
@@ -1,12 +1,12 @@
 require'nvim-treesitter.configs'.setup {
   ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
   highlight = {
-    enable = true,              -- false will disable the whole extension
+    enable = true,
   },
   indent = {
-    enable = true,              -- false will disable the whole extension
+    enable = false,
   },
   incremental_selection = {
-    enable = true,              -- false will disable the whole extension
+    enable = false,
   },
 }
